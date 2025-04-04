f8412b04384160c207676e92127a93ac0e10d9c0
FlorianAlguazil <florian.alguazil@gmail.com>
2025-04-04 18:51:42 +0200

    ajout des tests pour les voyages spaciaux

--- src/classes/spaciale/vaisseau.rs
+++ src/classes/spaciale/vaisseau.rs
@@ -3,32 +3,55 @@ use crate::classes::spaciale::planete::Planete;
 pub struct Vaisseau {
     carburant: i32,
     uranium: i32,
-    position: Option<Planete>, // La planète actuelle du vaisseau (None = espace)
+    position: Option<Planete>,
 }
 
 impl Vaisseau {
+    /// Crée un nouveau vaisseau avec du carburant, de l'uranium et une position initiale
     pub fn new(carburant: i32, uranium: i32, position: Option<Planete>) -> Self {
         Self { carburant, uranium, position }
     }
 
+    /// Affiche l'état actuel du vaisseau dans la console
     pub fn afficher_etat(&self) {
-
         let position_str = match &self.position {
             Some(planete) => format!("Sur {}", planete.nom),
             None => "Dans l'espace".to_string(),
         };
-        println!("Carburant: {}, Uranium: {}, Position: {}", self.carburant, self.uranium, position_str);
+        println!(
+            "Carburant: {}, Uranium: {}, Position: {}",
+            self.carburant, self.uranium, position_str
+        );
     }
 
+    /// Permet de voyager vers une planète si le carburant est suffisant
     pub fn voyager(&mut self, planete: &Planete) -> bool {
         if self.carburant >= planete.cout_voyage {
             self.carburant -= planete.cout_voyage;
             self.position = Some(planete.clone());
-            println!("Voyage réussi vers {} ! Carburant restant: {}", planete.nom, self.carburant);
+            println!(
+                "Voyage réussi vers {} ! Carburant restant: {}",
+                planete.nom, self.carburant
+            );
             true
         } else {
             println!("Pas assez de carburant pour aller sur {} !", planete.nom);
             false
         }
+    }
+
+    /// Retourne le carburant actuel
+    pub fn get_carburant(&self) -> i32 {
+        self.carburant
+    }
+
+    /// Retourne l'uranium actuel
+    pub fn get_uranium(&self) -> i32 {
+        self.uranium
+    }
+
+    /// Retourne la position actuelle du vaisseau
+    pub fn get_position(&self) -> Option<&Planete> {
+        self.position.as_ref()
     }
 }
